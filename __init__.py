0cdc72d4127c66e6df5f018a7f12caf8949e73ac
Jonathan Zweigle <jozweigle@gmail.com>
2026-08-14 09:56:55 +0200

    update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,19 +3,6 @@ PFAScreen - PFAS screening and analysis package
 """
 
 __version__ = "1.0.0"
-__author__ = "Your Name"
-
-# Import main functions for easy access
-from .screening import pfascreen  # renamed from pfascreen.py to screening.py
-from .params import get_config
-from .run_oms_pipeline import run_oms_pipeline
-from .pfascreen_utils.utils import filter_df, fold_change_filter
-from .pfascreen_utils.plotting import (
-    kmd_plot, scatter_plot, feature_overview_plot
-)
-from .pfascreen_utils.generate_html import generate_full_html
-from .networks import molecular_network, mass_difference_network
-from .readers_writers import msdial_alignmenttable_to_df
 
 __all__ = [
     'pfascreen',
